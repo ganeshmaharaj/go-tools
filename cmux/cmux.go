6ac29d3053283b12af0e09972a0a9736d5414f53
Ganesh Maharaj Mahalingam <ganesh.mahalingam@intel.com>
2018-05-25 11:55:05 -0700

    Add new connection flag to cmux
    Signed-off-by: Ganesh Maharaj Mahalingam <ganesh.mahalingam@intel.com>

--- cmux/cmux.go
+++ cmux/cmux.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -11,6 +12,8 @@ import (
 )
 
 func main() {
+
+	// Variables for the program
 	env := os.Environ()
 	var cmdout []byte
 	var sess []string
@@ -24,28 +27,37 @@ func main() {
 		return
 	}
 
-	cmd := exec.Command(tmuxbin, tmuxlsarg...)
-	cmdout, _ = cmd.Output()
-	sess = strings.Split(strings.TrimRight(string(cmdout), "\n"), "\n")
-	switch len(sess) {
-	case 1:
-		tmuxconnarg = append(tmuxconnarg, "attach", "-t", sess[0])
-	case 0:
-		break
-	default:
-		fmt.Println("Index 	Session")
-		fmt.Println("==================")
-		for idx, val := range sess {
-			fmt.Printf("%d	%s\n", idx, val)
+	// Input flags. Currently supports creating new connection
+	newconn := flag.Bool("n", false, "Create new connection")
+	flag.Parse()
+
+	// Establish tmux connection
+	if *newconn == true {
+		tmuxconnarg = []string{"-2"}
+	} else {
+		cmd := exec.Command(tmuxbin, tmuxlsarg...)
+		cmdout, _ = cmd.Output()
+		sess = strings.Split(strings.TrimRight(string(cmdout), "\n"), "\n")
+		switch len(sess) {
+		case 1:
+			tmuxconnarg = append(tmuxconnarg, "attach", "-t", sess[0])
+		case 0:
+			break
+		default:
+			fmt.Println("Index 	Session")
+			fmt.Println("==================")
+			for idx, val := range sess {
+				fmt.Printf("%d	%s\n", idx, val)
+			}
+			fmt.Print("Session >> ")
+			buffin.Scan()
+			userval, err = strconv.Atoi(buffin.Text())
+			if err != nil {
+				fmt.Println("Unable to parse user input")
+				return
+			}
+			tmuxconnarg = append(tmuxconnarg, "attach", "-t", sess[userval])
 		}
-		fmt.Print("Session >> ")
-		buffin.Scan()
-		userval, err = strconv.Atoi(buffin.Text())
-		if err != nil {
-			fmt.Println("Unable to parse user input")
-			return
-		}
-		tmuxconnarg = append(tmuxconnarg, "attach", "-t", sess[userval])
 	}
 
 	err = syscall.Exec(tmuxbin, tmuxconnarg, env)
